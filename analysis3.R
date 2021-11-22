86894aacb43a2aacee5224178181bcdf65b55114
rynao@uw.edu <rynao@uw.edu>
2021-11-21 22:11:10 -0800

    Ryan's Chart

---
+++ analysis3.R
@@ -0,0 +1,14 @@
+install.packages("readxl")
+library("readxl")
+library(ggplot2)
+library("stringr")
+library(dplyr)
+my_data <- read_excel(file.choose("twitchdata-update.xlsx"))
+
+scatter <- ggplot(my_data) +
+  geom_point(mapping = aes(x = watch_time, y = Followers, color = watch_time)) +
+  scale_color_continuous("Watch Time") +
+  labs(x = "Watch Time", y = "Followers") +
+  ggtitle("Watch Time Compared with Followers for Each Channel")
+
+View(scatter)
